976c6259e6784df304b76cab8f08ec9cacca2f14
Bert Keller <bertwinkeller@gmail.com>
2020-10-26 21:53:46 -0700

    used all array methods

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const dummyTransactions = [
     {
         id: 3,
         text: 'Salary',
-        amount: 500
+        amount: 120
     },
     {
         id: 4,
@@ -63,8 +63,23 @@ processArray(dummyTransactions)
 //update the balance, income and expense
 function updateValues(){
 
-    const amounts = dummyTransactions.map(ttransaction => transaction.amount)
+    const amounts = dummyTransactions.map(transaction => transaction.amount)
 
-    console.log(amounts)
+
+    const total = amounts.reduce((acc, item) => (acc +=item), 0).toFixed(2)
+
+
+
+    const income = amounts.filter(item => item > 0)
+                            .reduce((acc, item) => (acc += item),0)
+                            .toFixed(2)
+
+    const expenses = (amounts.filter(item => item < 0)
+    .reduce((acc, item) => (acc += item),0)*-1)
+    .toFixed(2)
+
+    money_plus.innerText = `+$${income}`
+    money_minus.innerText = `-$${expenses}`
+    balance.innerText = `$${total}`
 
 }
